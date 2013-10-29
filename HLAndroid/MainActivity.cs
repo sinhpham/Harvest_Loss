1da789354f9796d1791970d7a4545f0617edf502
Sinh Pham <phamansinh@gmail.com>
2013-10-29 17:10:20 -0600

    UI Android.

--- HLAndroid/MainActivity.cs
+++ HLAndroid/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Com.Slidingmenu.Lib.App;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HLAndroid
 {
@@ -37,12 +39,28 @@ namespace HLAndroid
             SlidingMenu.SetBehindWidth(500);
             SlidingMenu.SetFadeDegree(0.35f);
             SlidingMenu.TouchModeAbove = Com.Slidingmenu.Lib.SlidingMenu.TouchmodeFullscreen;
-
         }
 
         public void SelectedItemChanged(int position, string label)
         {
             Console.WriteLine(label);
+        }
+
+        private void CreateSelectDialog<T>(IList<T> list, Func<T, string> display, string title, Action<T> selectedAction)
+        {
+            // Return selected index
+            var displayList = list.Select(display).ToArray();
+            var adap = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, displayList);
+
+            new AlertDialog.Builder(this)
+            .SetTitle(title)
+            .SetSingleChoiceItems(adap, -1, (s, arg) =>
+            {
+                var ad = (AlertDialog)s;
+                var pos = ad.ListView.CheckedItemPosition;
+                ad.Dismiss();
+                selectedAction(list[pos]);
+            }).Create().Show();
         }
     }
 }
