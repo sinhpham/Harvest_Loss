7c2604f5c255b51f90363993eaabd1f8a5c27ac3
Sinh Pham <phamansinh@gmail.com>
2013-10-29 17:23:19 -0600

    Choosing item.

--- HLAndroid/MainActivity.cs
+++ HLAndroid/MainActivity.cs
@@ -8,14 +8,14 @@ using Android.OS;
 using Com.Slidingmenu.Lib.App;
 using System.Collections.Generic;
 using System.Linq;
+using Java.Interop;
+using HLIOSCore;
 
 namespace HLAndroid
 {
     [Activity(Label = "HLAndroid", MainLauncher = true)]
     public class MainActivity : SlidingFragmentActivity, MenuFragment.ISlidingMenuAct
     {
-        int count = 1;
-
         public override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -35,15 +35,43 @@ namespace HLAndroid
 
             // Setup the sliding menu
             SlidingMenu.SetShadowWidth(50);
-            SlidingMenu.SetShadowDrawable (Resource.Drawable.SlidingMenuShadow);
+            SlidingMenu.SetShadowDrawable(Resource.Drawable.SlidingMenuShadow);
             SlidingMenu.SetBehindWidth(500);
             SlidingMenu.SetFadeDegree(0.35f);
             SlidingMenu.TouchModeAbove = Com.Slidingmenu.Lib.SlidingMenu.TouchmodeFullscreen;
+
+
+
+            //HLDatabase.CreateDummyData(dataLines);
         }
 
         public void SelectedItemChanged(int position, string label)
         {
+            // TODO: handle menu item changed.
             Console.WriteLine(label);
+        }
+
+        [Export]
+        public void MethodClicked(View v)
+        {
+            var l = new List<string> { "Weight", "Volume", "Count" };
+            CreateSelectDialog(l, x => x, "Select method", selected =>
+            {
+
+            });
+        }
+
+        [Export]
+        public void CropClicked(View v)
+        {
+            var l = new List<string>();
+            for(var i = 0; i < 100; ++i) {
+                l.Add(i.ToString());
+            }
+            CreateSelectDialog(l, x => x, "Select method", selected =>
+            {
+
+            });
         }
 
         private void CreateSelectDialog<T>(IList<T> list, Func<T, string> display, string title, Action<T> selectedAction)
@@ -53,8 +81,8 @@ namespace HLAndroid
             var adap = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, displayList);
 
             new AlertDialog.Builder(this)
-            .SetTitle(title)
-            .SetSingleChoiceItems(adap, -1, (s, arg) =>
+                .SetTitle(title)
+                    .SetSingleChoiceItems(adap, -1, (s, arg) =>
             {
                 var ad = (AlertDialog)s;
                 var pos = ad.ListView.CheckedItemPosition;
