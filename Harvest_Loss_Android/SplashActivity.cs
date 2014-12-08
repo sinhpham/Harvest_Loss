539e62323d6c01e9b73ec2d1d9dfd0a7a6ed2a12
Sinh Pham <phamansinh@gmail.com>
2014-12-08 16:24:11 -0600

    Update packages + splash screen.

---
+++ Harvest_Loss_Android/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Harvest_Loss_Android
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
+
